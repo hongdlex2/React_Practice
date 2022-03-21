66c37a4bb5ee6cce2eabd7cf1836b3a138990c97
hongdlex2 <ghdals1997@naver.com>
2022-03-21 16:12:44 +0900

    event practice

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header(props) {
-  return <header>
-    <h1><a href='/'>{props.title}</a></h1>
-  </header>
-}
-function Nav(props){
-  const list = []
-  for(let i=0; i<props.topics.length; i++){
-    let t = props.topics[i];
-    list.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
-  }
-  return <nav>
-    <ol>
-      {list}
-    </ol>  
-  </nav>
-}
 function Article(props){
   return<article>
     <h2>{props.title}</h2>
     {props.body}
   </article>
+}
+
+function Header(props) {
+  return <header>
+    <h1><a href='/' onClick={function(event){
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+
+function Nav(props){
+  const list = []
+
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    list.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={function(event){
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+      </li>)
+  }
+
+  return <nav>
+    <ol>
+      {list}
+    </ol>  
+  </nav>
 }
 
 
@@ -35,8 +47,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title='React'></Header>
-      <Nav topics={topics}></Nav>
+      <Header title='React' onChangeMode={function(){
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={function(id){
+        alert(id);
+      }}></Nav>
       <Article title='Welcome' body="Hello, WEB"></Article>
     </div>   
   );
